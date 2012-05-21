88c2afd726add4fe1e2b8cd117d148bce483e214
Stevan Dedovic <stevanproductions@gmail.com>
2012-05-20 22:12:10 -0500

    messing around with jbox2d

--- src/com/dedovic/bots/Bot.java
+++ src/com/dedovic/bots/Bot.java
@@ -1,6 +1,7 @@
 package com.dedovic.bots;
 
 import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
@@ -9,24 +10,26 @@ import org.jbox2d.dynamics.World;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public class Bot {
-	private int health, rotation;
-	private Shape body;
+	private int health;
 	private World world;
 	private BodyDef botBodyDef;
 	private PolygonShape botShape;
 	private Body botBody;
 	private FixtureDef botFixtureDef;
-	protected float xPos, yPos;
+	private Rectangle rect;
+	private static final float WIDTH = 1f;
+	private static final float  HEIGHT = 1f;
+	
 	
 	public Bot(float x, float y, World world){
-		xPos = x;
-		yPos = y;
 		health = 100;
+		rect = new Rectangle(x*10, y*10, WIDTH*100, HEIGHT*100);				
 		
 		this.world = world;
 		botBodyDef = new BodyDef();
@@ -34,7 +37,7 @@ public class Bot {
 		botBodyDef.position.set(x, y);
 		botBody = world.createBody(botBodyDef);
 		botShape = new PolygonShape();
-		botShape.setAsBox(1f, 1f);
+		botShape.setAsBox(WIDTH, HEIGHT);
 		
 		botFixtureDef = new FixtureDef();
 		botFixtureDef.shape = botShape;
@@ -45,13 +48,23 @@ public class Bot {
 	}
 	
 	public void update(GameContainer container, int delta) throws SlickException {
-		//TODO: Death handling
-		//TODO: Collision handling
-		//TODO: Act
+		Input input = container.getInput();
+		float strength = 30f;
+		
+		if(input.isKeyPressed(Input.KEY_W))
+			botBody.applyForce(botBody.getPosition(), new Vec2(0, strength));
+		if(input.isKeyPressed(Input.KEY_S))
+			botBody.applyForce(botBody.getPosition(), new Vec2(0, -strength));
+		if(input.isKeyPressed(Input.KEY_D))
+			botBody.applyForce(botBody.getPosition(), new Vec2(strength, 0));
+		if(input.isKeyPressed(Input.KEY_A))
+			botBody.applyForce(botBody.getPosition(), new Vec2(-strength, 0));
+		
+		rect.setLocation(botBodyDef.position.x*10, botBodyDef.position.y*10);
 			
 	}
 	public void render(GameContainer container, Graphics g) throws SlickException {
 		g.setColor(new Color(health, health, health));
-		g.fill(body);
+		g.fill(rect);
 	}
 }
